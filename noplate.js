590da7aeefbe3669a68f861364837107a5c05d92
Andy Gayton <andy@thecablelounge.com>
2011-01-11 21:09:57 -0800

    add the ability to link externally to a given noplate page

--- noplate.js
+++ noplate.js
@@ -3,13 +3,20 @@ $(function(){
         var ob = $(this);
         $.get(ob.attr('href'), {}, function(data){
             $(ob.attr('target')).html(data);
+            $.address.value(ob.attr('href'));
         });
         return false;
     });
+
     $('.noplate-initial').each(function(){
         var ob = $(this);
         $.get(ob.attr('data-href'), {}, function(data){
             ob.html(data);
         });
     });
+
+    var page = $.address.value().substr(1);
+    if(page){
+        $('a[href$="'+page+'"]').click();
+    }
 });
