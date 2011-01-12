e98fadc126085b46aec24e5fec722cfff71ece9d
Andy Gayton <andy@thecablelounge.com>
2011-01-11 23:25:20 -0800

    updates for relative urls

--- noplate.js
+++ noplate.js
@@ -1,16 +1,16 @@
 $(function(){
     function load_href(href, target, set_address) {
-        var filename = href.substr(3, href.length-3) + '.html';
+        var filename = href.substr(2, href.length-2) + '.html';
         $.get(filename, {}, function(data){
             target.html(data);
-            if(set_address) $.address.value(href.substr(3));
+            if(set_address) $.address.value(href.substr(2));
         });
     }
     $('.noplate-load').live('click', function(){
         load_href($(this).attr('href'), $($(this).attr('target')), true);
         return false;
     });
-    var page = $('a[href$="'+'/#'+$.address.value()+'"]');
+    var page = $('a[href$="'+'#'+$.address.value()+'"]');
     if(page.length){
         page.click();
     } else {
